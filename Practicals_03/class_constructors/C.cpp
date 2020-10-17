843890dd71c0ceed599d8b32c81bee145497572f
root <root@mkyjovsky01.localdomain>
2020-10-17 11:29:22 +0200

    Assignemt 03 completed

---
+++ Practicals_03/class_constructors/C.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mkyjovsky01 on 10/14/2020.
+//
+#include <iostream>
+#include "C.hpp"
---
+++ Practicals_03/comlex_number_class/complex_number.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mkyjovsky01 on 10/17/2020.
+//
+
+#include "complex_number.hpp"
