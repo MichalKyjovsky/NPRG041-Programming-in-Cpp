21af3863ea0cf646b9fa0dc6f07900caaef3b96c
mkyjovsky <michal.kyjovsky@getmanta.com>
2020-11-07 16:16:51 +0100

    Added solution for practicals 05

---
+++ Practicals_05/num_vector.hpp
@@ -0,0 +1,55 @@
+//
+// Created by mkyjovsky01 on 11/7/2020.
+//
+
+#ifndef ARGUMENTS_CALC_NUM_VECTOR_HPP
+#define ARGUMENTS_CALC_NUM_VECTOR_HPP
+
+#include <memory>
+#include <iostream>
+
+class NumVector {
+    // This is known to the user
+    std::size_t current_size;
+    // This is allocated on the background
+    std::size_t current_capacity;
+    std::unique_ptr<int[]> vec_ptr;
+
+public:
+    NumVector(std::size_t capacity) : current_capacity(2 * capacity) {
+        this->vec_ptr = std::make_unique<int[]>(this->current_capacity);
+        this->current_size = capacity;
+    }
+
+    NumVector() {
+        this->current_capacity = 2;
+        this->current_size = 1;
+        this->vec_ptr = std::make_unique<int[]>(this->current_capacity);
+    }
+
+    int &operator[](std::size_t idx) {
+        if (idx < this->current_size) {
+            return vec_ptr[idx];
+        } else {
+            throw std::out_of_range("Index out of bound of range!");
+        }
+    }
+
+    const int &operator[](std::size_t idx) const {
+        if (idx < this->current_size) {
+            return vec_ptr[idx];
+        } else {
+            throw std::out_of_range("Index out of bound of range!");
+        }
+    }
+
+    std::size_t capacity();
+    std::size_t size() const;
+    void reserve(std::size_t new_cap);
+    void push_back(int value);
+    void resize(std::size_t count);
+    void print();
+};
+
+
+#endif //ARGUMENTS_CALC_NUM_VECTOR_HPP
